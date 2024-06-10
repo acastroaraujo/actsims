e7d29010c219ba51eaaf288cf6e5305d0b853409
andrés <acastroaraujo@gmail.com>
2024-06-10 17:51:29 -0400

    added two subsetting methods for event_deflection obejects (for parallel subsetting of attributes)

--- R/actsims-package.R
+++ R/actsims-package.R
@@ -3,6 +3,7 @@
 
 ## usethis namespace: start
 #' @importFrom pillar tbl_format_header
+#' @importFrom dplyr dplyr_row_slice
 #' @importFrom R6 R6Class
 #' @importFrom rlang .data
 ## usethis namespace: end
--- R/deflection-attributes.R
+++ R/deflection-attributes.R
@@ -30,3 +30,55 @@ get_element_wise_deflection <- function(x) {
   attr(x, "element_wise_deflection", exact = TRUE)
 }
 
+
+
+# Filter and Subset -------------------------------------------------------
+
+## This is experimental.
+## I want the attributes of the event_deflection data frame to change
+## accordingly each time I subset it.
+## But... this requires that I create a bunch of generic methods, that will not
+## extend to other packages.
+## So what I'm thinking right now is that maybe the attributes should be stored
+## in the deflection column... that way, every time I subset I don't have to
+## worry about what happens to the attributes. This is probably what I will do.
+
+
+#' @export
+dplyr_row_slice.event_deflection <- function(data, i, ...) {
+
+  out <- dplyr::dplyr_row_slice(dplyr::as_tibble(data), i)
+  tra <- dplyr::dplyr_row_slice(get_transients(data), i)
+  fun <- dplyr::dplyr_row_slice(get_fundamentals(data), i)
+  ewd <- dplyr::dplyr_row_slice(get_element_wise_deflection(data), i)
+
+  ## Parallel filtering output
+  structure(
+    out,
+    class = c("event_deflection", class(out)),
+    element_wise_deflection = ewd,
+    transients = tra,
+    fundamentals = fun
+  )
+}
+
+
+#' @export
+`[.event_deflection` <- function(data, i, j, ..., drop = TRUE) {
+
+  out <- NextMethod("[")
+  tra <- get_transients(data)[i, ]
+  fun <- get_fundamentals(data)[i, ]
+  ewd <- get_element_wise_deflection(data)[i, ]
+
+  structure(
+    out,
+    class = class(out),
+    element_wise_deflection = ewd,
+    transients = tra,
+    fundamentals = fun
+  )
+
+}
+
+
